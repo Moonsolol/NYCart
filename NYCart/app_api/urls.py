48e65faa070a688fa76ee0fb4a97eba38519e638
Moonsolol <moonsolol@yahoo.com>
2022-06-29 18:15:03 -0400

    added GET, PATCH and DELETE to API and created useradmin

--- NYCart/app_api/urls.py
+++ NYCart/app_api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import VendorView
 
 urlpatterns = [
-    path('vendors/', VendorView.as_view())
+    path('vendors/', VendorView.as_view()),
+    path('vendors/<int:id>', VendorView.as_view())
 ]
--- NYCart/app_api/views.py
+++ NYCart/app_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .serializers import VendorSerializer
 from .models import Vendor
 # Create your views here.
@@ -14,3 +15,26 @@ class VendorView(APIView):
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, id=None):
+        if id:
+            vendor = Vendor.objects.get(id=id)
+            serializer = VendorSerializer(vendor)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        vendors = Vendor.objects.all()
+        serializer = VendorSerializer(vendors, many=True)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+    def patch(self, request, id=None):
+        vendor = Vendor.objects.get(id=id)
+        serializer = VendorSerializer(vendor, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+        else:
+            return Response({"status": "error", "data": serializer.errors})
+
+    def delete(self, request, id=None):
+        vendor = get_object_or_404(Vendor, id=id)
+        vendor.delete()
+        return Response({"status": "success", "data": "Item Deleted"})
